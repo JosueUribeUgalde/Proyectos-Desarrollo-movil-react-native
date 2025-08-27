dd659862cd474ef61a2ab9d89ec052842d9a4baa
Josue uribe ugalde <josue86443@gmail.com>
2025-08-27 07:49:52 -0600

    feat: Agregar campos de entrada para gasto y monto en la app de control de gastos

--- ControlGastos/App.js
+++ ControlGastos/App.js
@@ -1,9 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TextInput } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+const [gasto, setgasto] = useState('');
+const [monto, setmonto] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text style={style.title}>App de control de gastos</Text>
+      <Text style={styles.title}>App de control de gastos</Text>
+      <TextInput
+      style={styles.input}
+      placeholder='Ingresa el titulo de gasto'
+      value={gasto}
+      onChangeText={setgasto}
+      />
+            <TextInput
+      style={styles.input}
+      placeholder='Ingresa el monto de gasto'
+      value={monto}
+      onChangeText={setmonto}
+      keyboardType='numeric'
+      />
     </View>
   );
 }
@@ -13,12 +30,20 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    padingTop: 20,
   },
   title: {
+    paddingTop: 80,
     fontSize: 20,
     fontWeight: 'bold',
     color: '#000000ff',
     textAlign: 'center',
+  },
+  input: {
+    width: '80%',
+    borderColor: '#000000ff',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginVertical: 10,
   },
 });
