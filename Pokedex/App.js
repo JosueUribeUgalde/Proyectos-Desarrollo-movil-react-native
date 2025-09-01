729161b6e8338ea8f7833efc73369198b1eb69ce
lordgovenate <kalonso17@alumnos.uaq.mx>
2025-09-01 08:00:35 -0600

    feat: Agregar interfaz para ingresar datos de Pokémon en la app Pokédex

--- Pokedex/App.js
+++ Pokedex/App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, TouchableOpacity} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.textTitle}>Pokédex</Text>
+    <View style={styles.containerInputs}>
+      <TextInput style={styles.textImput} placeholder="Ingresa el nombre del pokemon" />
+      <TextInput style={styles.textImput} placeholder="Ingresa el tipo del pokemon" />
+      <TextInput style={styles.textImput} placeholder="Ingresa la url del pokemon" />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Agregar Pokemon</Text>
+      </TouchableOpacity>
+    </View>
     </View>
   );
 }
@@ -15,6 +21,35 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    height: '100%',
+  },
+  textTitle: {
+    fontSize: 30,
+  },
+  textImput: {
+    height: 45,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingLeft: 20,
+    fontSize: 16,
+  },
+  containerInputs: {
+    marginTop: 20,
+    width: '80%',
+    justifyContent: 'space-between',
+    height: '30%',
+  },
+  button: {
+    backgroundColor: '#000000ff',
+    padding: 10,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
